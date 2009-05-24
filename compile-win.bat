1babdc4aaf43539e2af91ba06785a10a005630c0
FooBar <foobar@jaspervries.nl>
2009-05-25 00:44:01 +0200

    Initial repo population. Although restructured, this should produce 0.1.0-alpha4.2.

---
+++ compile-win.bat
@@ -0,0 +1,11 @@
+CD ../
+
+grfcodec.exe -e -p 2 ogfx1.grf ogfx/sprites/
+grfcodec.exe -e -p 2 ogfxc.grf ogfx/sprites/
+grfcodec.exe -e -p 2 ogfxh.grf ogfx/sprites/
+grfcodec.exe -e -p 2 ogfxi.grf ogfx/sprites/
+grfcodec.exe -e -p 2 ogfxt.grf ogfx/sprites/
+grfcodec.exe -e -p 2 ogfxe.grf ogfx/sprites/
+
+PAUSE
+EXIT
