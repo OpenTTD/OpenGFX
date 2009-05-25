4d38fddf46c76c06e58cf27070669927fa4a031e
planetmaker <planetmaker@openttd.org>
2009-05-25 09:16:20 +0200

    Fix #83: make OpenGFX compile independent of the directory name it resides in. Note: put the dir grfcodec.exe resides in, into your path environment variable

--- compile-win.bat
+++ compile-win.bat
@@ -1,10 +1,13 @@
-CD ../
-
-grfcodec.exe -e -p 2 ogfx1.grf ogfx/sprites/
-grfcodec.exe -e -p 2 ogfxc.grf ogfx/sprites/
-grfcodec.exe -e -p 2 ogfxh.grf ogfx/sprites/
-grfcodec.exe -e -p 2 ogfxi.grf ogfx/sprites/
-grfcodec.exe -e -p 2 ogfxt.grf ogfx/sprites/
-grfcodec.exe -e -p 2 ogfxe.grf ogfx/sprites/
+grfcodec -e -p 2 ogfx1.grf sprites/
+echo "Now processing ogfxc"
+grfcodec -e -p 2 ogfxc.grf sprites/
+echo "Now processing ogfxh"
+grfcodec -e -p 2 ogfxh.grf sprites/
+echo "Now processing ogfxi"
+grfcodec -e -p 2 ogfxi.grf sprites/
+echo "Now processing ogfxt"
+grfcodec -e -p 2 ogfxt.grf sprites/
+echo "Now processing ogfxe"
+grfcodec -e -p 2 ogfxe.grf sprites/
 
 PAUSE
